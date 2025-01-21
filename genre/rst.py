c117630396738bd0ad88b7868a0876f288818c33
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-01-21 05:38:12 -0500

    Add Board Instance ID ReST doc generation

--- genre/rst.py
+++ genre/rst.py
@@ -12,6 +12,17 @@ Board Type IDs
      - Board Name
      - ID"""
 
+board_inst_id_str = """
+Board Instance IDs
+******************"""
+
+board_inst_table_header = """   :widths: 25 60 15
+   :header-rows: 1
+
+   * - Enum Name
+     - Board Instance Name
+     - ID"""
+
 def gen_board_id_rst(rocketcan):
     print(board_id_rst_header)
 
@@ -22,3 +33,18 @@ def gen_board_id_rst(rocketcan):
         else:
             print('     - ' + board['desc'].data)
         print('     - 0x' + '{:02X}'.format(board['id'].data))
+
+    print(board_inst_id_str)
+
+    inst_id = 2
+    for board in rocketcan['boards']:
+        if "inst" in board:
+            print('\n' + board['desc'].data)
+            print('=' * len(board['desc'].data) + '\n')
+            print('.. list-table:: ' + board['desc'].data + ' Instances')
+            print(board_inst_table_header)
+            for inst in board['inst']:
+                print('   * - ' + inst['name'].data)
+                print('     - ' + inst['desc'].data)
+                print('     - 0x' + '{:02X}'.format(inst_id))
+                inst_id += 1
